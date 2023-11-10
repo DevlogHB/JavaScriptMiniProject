81b2144ac16e30c723c0ebfec5ae0072f299332a
HongBin <itshb1221@gmail.com>
2023-11-10 12:05:12 +0900

    add css, auth, hosting

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -13,8 +17,26 @@ const passport = require("passport");
 const LoaclStrategy = require("passport-local");
 const User = require("./models/user.js");
 const userRoutes = require("./routers/users");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelpCamp";
+const MongoStore = require("connect-mongo");
 
-mongoose.connect("mongodb://localhost:27017/yelpCamp", {
+const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
+// mongoose.connect("mongodb://localhost:27017/yelpCamp", {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -35,12 +57,20 @@ app.use(express.urlencoded({ extends: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 const sessionConfig = {
-  secret: "thisshouldbeabettersecret!",
+  store,
+  name: "session",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
@@ -48,6 +78,12 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    xDownloadOptions: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
--- yelpCamp/seeds/index.js
+++ yelpCamp/seeds/index.js
@@ -27,9 +27,18 @@ const seedDB = async () => {
       author: "654b26c1cd975731849488d2",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
       description: "description",
       price: price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png",
+          filename: "YelpCamp/ahfnenvca4tha00h2ubt",
+        },
+        {
+          url: "https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png",
+          filename: "YelpCamp/ruyoaxgf72nzpi4y6cdi",
+        },
+      ],
     });
     await camp.save();
   }
